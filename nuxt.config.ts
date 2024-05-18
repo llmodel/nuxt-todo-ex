4d5c17b69ac2f3f27dfb9f9caa97875059a14c1a
Open Source Developer <llgm759@gmail.com>
2024-05-17 21:42:50 -0700

    configured pinia in nuxt.config.tx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss",
     "nuxt-primevue",
+    "@pinia/nuxt",
   ],
   // primevue: {
   //   cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
